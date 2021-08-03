972aa2410ad0f481d2e9b7d55eb42c6dcd666038
kitayka <kitayka96@gmail.com>
2021-08-03 21:41:31 +0300

    1

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView, UpdateView, TemplateView
+from django.contrib.auth import get_user_model
+from django.core.signing import BadSignature
 
-# Create your views here.
+from .forms import AccountRegistrationForm
+from .utils import signer
+
+
+class AccountRegistrationView(CreateView):
+    model = get_user_model()
+    templates = 'accounts/registration.html'
+    success_url = reverse_lazy('account:registration_done')
+    form_class = AccountRegistrationForm
+
+
+class AccountRegistrationDoneView(TemplateView):
+    template_name = 'accounts/registration_done.html'
+
+
+def user_activate(request, sign):
+    try:
+        username = signer.unsign(sign)
+    except BadSignature:
+        return render(request, 'accounts/bad_signature.html')
+
+    user = get_object_or_404(get_user_model(), username=username)
+    if user.is_activated:
+        templates = 'accounts/user_is_activated.html'
+    else:
+        templates = 'accounts/activation_done.html'
+        user.is_active = True
+        user.is_activated = True
+        user.save()
+
+    return render(request, templates)
--- thequestion/settings.py
+++ thequestion/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'thequestion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,9 +118,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_PORT = 1025
